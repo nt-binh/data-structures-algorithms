a642051756e6c9729a4b7d84557426b62eaf6d96
nt-binh174 <binh.nt1741996@gmail.com>
2022-09-17 13:03:47 +0900

    Add comment for LongestSubstringWithKDistinctCharacters

--- src/main/java/com/ntbinh/algods/algorithms/array/LongestSubstringWithKDistinctCharacters.java
+++ src/main/java/com/ntbinh/algods/algorithms/array/LongestSubstringWithKDistinctCharacters.java
@@ -20,18 +20,35 @@ public class LongestSubstringWithKDistinctCharacters {
         Map<Character, Integer> freq = new HashMap<>();
         for (int i = 0; i < input.length(); i++) {
             char currChar = input.charAt(i);
+
+            // Check whether the frequency table already has the current character or not
             if (freq.containsKey(currChar)) {
+                // Update the frequency of the character by increasing it by 1
                 freq.put(currChar, freq.get(currChar) + 1);
             } else {
                 freq.put(currChar, 1);
                 while (freq.size() > k) {
                     char tempChar = input.charAt(start);
+                    
+                    // By decreasing the frequency of the character indexed at start
+                    // by 1, I remove that character from the substring
                     freq.put(tempChar, freq.get(tempChar) - 1);
-                    if (freq.get(tempChar) == 0) freq.remove(tempChar);
+                    
+                    // Since I remove the character indexed at start,
+                    // Increase the start by 1
                     start++;
+
+                    // If the character has no appearance in the substring
+                    // then remove it from the frequency table
+                    // Therefore, the size of the frequency will be exactly
+                    // the same as the number of distinct letters in the substring
+                    if (freq.get(tempChar) == 0) freq.remove(tempChar);
                 }
             }
+            // Update the end point of current substring
             end++;
+
+            // Update length of the current longest substring
             max = Math.max(end - start + 1, max);
         }
         System.out.println("The longest substring with K distinct characters is " + max);
